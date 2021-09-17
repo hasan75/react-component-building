5e1bd2817d344b73e497eab82a530e57ca1e5989
Hasan75 <hasanjab14@gmail.com>
2021-09-18 03:08:16 +0600

    data added dynamically from an array

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  
+  const products = [
+    {name: 'Mobile', price: 15000},
+    {name: 'Watch', price: 1900},
+    {name: 'Smart Band', price: 1500},
+    {name: 'Laptop', price: 120000},
+    {name: 'Camera', price: 50000},
+  ]
   return (
     <div className="App">
-      <Product name="Mobile" price="15000"></Product>
-      <Product name="Laptop" price="150000"></Product>
-      
+      { products.map(product => <Product name={product.name} price={product.price}></Product>) }
     </div>
   );
 }
 
 function Product(props){
+  console.log(props)
   const productStyle = {
     border: '3px solid grey',
     borderRadius: '20px',
