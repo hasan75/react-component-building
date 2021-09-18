3d92f9875103fd60a5bd84c721dedeb38b264b66
Hasan75 <hasanjab14@gmail.com>
2021-09-18 15:49:05 +0600

    api called in react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { useEffect } from 'react/cjs/react.development';
 
 function App() {
   
   return (
+ 
+
     <div className="App">
       <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
     </div>
+  
   );
 }
+
+function ExternalUsers () {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+
+  return (
+    <div>
+      <h2>External Users</h2>
+      {users.map(user => <User name={user.name} email={user.email}></User>)}
+    </div>
+  )
+}
+
+function User (props){
+  const userStyle = {backgroundColor:'lightblue', padding: '20px', margin: '20px', border: '3px solid grey', borderRadius: '15px'}
+  return(
+    <div style={userStyle} >
+      <h3>Name:{props.name}</h3>
+      <p>Email:{props.email}</p>
+    </div>
+  )
+}
+
+
+
 
 function Counter (){
   const [count, setCount] = useState(10);
@@ -20,8 +54,8 @@ function Counter (){
   return(
     <div style={{backgroundColor:'lightblue', padding: '20px', margin: '20px', border: '3px solid grey', borderRadius: '15px'}}>
       <h3>Count:{count}</h3>
-      <button onClick={increaseHandler}>Increase</button>
-      <button onClick={decreaseHandler}>Decrease</button>
+      <button style={{padding: '5px', margin: '15px'}} onClick={increaseHandler}>Increase</button>
+      <button style={{padding: '5px', margin: '15px'}} onClick={decreaseHandler}>Decrease</button>
     </div>
   )
 }
@@ -75,3 +109,32 @@ function Counter(){
   */
 
 export default App;
+
+/*
+function ExternalUsers(){
+  const [users, setUsers] = useState([]);
+  useEffect(()=> {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+  return(
+    <div>
+      <h3>External Users</h3>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User (props) {
+  return(
+    <div className="product">
+      <h2>Name: {props.name}</h2>
+      <p>Email: {props.email}</p>
+    </div>
+  )
+}
+
+*/
